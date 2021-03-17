af4c9a0d7fc5a127b36eabb2dc112c803e15a555
Giancarlo Pacheco <g1an@hotmail.com>
2021-03-17 14:09:37 -0400

    make APIs public

--- Sources/CashCore/Model/User/UserDefaults+User.swift
+++ Sources/CashCore/Model/User/UserDefaults+User.swift
@@ -3,11 +3,11 @@ import Foundation
 
 extension UserDefaults {
     
-    func setUser(_ user: CoreUser) throws {
+    public func setUser(_ user: CoreUser) throws {
         try setObject(user, for: Keys.User.rawValue)
     }
     
-    func getUser<CoreUser>() throws -> CoreUser? where CoreUser: Decodable {
+    public func getUser<CoreUser>() throws -> CoreUser? where CoreUser: Decodable {
         return try getObject(for: Keys.User.rawValue)
     }
     
